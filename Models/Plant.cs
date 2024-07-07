c9a320484921b8594ab36ce2bc1f2a95f41de108
Cherryl Patel <cherrylptl2260@gmail.com>
2024-07-07 16:12:04 -0400

    Validation Implemented

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -1,13 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment2.Models
 {
     public class Plant
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? Species { get; set; }
-        public decimal? Price { get; set; }
-        public int? StockQuantity { get; set; }
 
+        [Required(ErrorMessage = "The Name is required.")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description is required.")]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "The Species is required.")]
+        public required string Species { get; set; }
+
+        [Required(ErrorMessage = "The Price is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter a valid price.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The Stock Quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid stock quantity.")]
+        public int StockQuantity { get; set; }
     }
 }
